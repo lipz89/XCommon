5fafe0ee26870f61f41f4bad29c2384d7c62a1c9
lipz <23955618@qq.com>
2018-06-27 11:06:00 +0800

    init

---
+++ XCommon/XCommon/Functions/Math.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace XCommon.Functions
+{
+    public static class Math
+    {
+        public static IEnumerable<int> AllPrimes()
+        {
+            var it = AllNaturalNumbers().Where(x => x > 1);
+            while (true)
+            {
+                var n = it.Take(1).FirstOrDefault();
+                yield return n;
+                it = it.Where(x => x % n > 0);
+            }
+        }
+
+        public static IEnumerable<int> AllNaturalNumbers()
+        {
+            var n = 0;
+            while (true)
+            {
+                yield return n;
+            }
+        }
+    }
+}
